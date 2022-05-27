9f0251f6c44ea28917ae23e44d593f5517897f2b
Sungjun <wltjdwnsg@naver.com>
2022-05-27 19:02:11 +0900

    image

--- client/src/components/TodoItem.js
+++ client/src/components/TodoItem.js
@@ -1,15 +1,19 @@
 // rfce ~~
 
 import React from 'react'
+import{ VscWand }from"react-icons/vsc";
+import{ VscTrash }from"react-icons/vsc";   
+
 
 function TodoItem() {
   return (
     <li className='todoApp_Item'>
 
-        <input type="check box" className='todoApp_Item-checkBox' />
+        <input type="checkbox" className='todoApp_Item-checkBox' />
         <div className='todoApp_item-contents'>ToDoList</div>
-        <button type='button' className='todoApp_item-addition'>추가</button>
-        <button type='button' className='todoApp_item-delete'>삭제</button>
+        <button type='button' className='todoApp_item-amend'><VscWand/></button>
+        <button type='button' className='todoApp_item-delete'><VscTrash/></button>
+        
 
     </li>
   )
